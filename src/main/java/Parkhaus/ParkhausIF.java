04c21531230368890d38a82a7cdf18959bd452f6
hheyen2s <helena.heyen@smail.inf.h-brs.de>
2023-05-25 14:12:45 +0200

    State Klassen überarbeitet

--- src/main/java/Parkhaus/ParkhausIF.java
+++ src/main/java/Parkhaus/ParkhausIF.java
@@ -2,4 +2,6 @@ package Parkhaus;
 
 public interface ParkhausIF {
     Ticket neuesTicket(String art) throws ParkplaetzeBelegtException;
+    public double bezahleTicket(Ticket t);
+    public String ausfahren(Ticket ticket) throws TicketNichtGefundenException;
 }
--- src/main/java/Parkhaus/StateAktiv.java
+++ src/main/java/Parkhaus/StateAktiv.java
@@ -40,39 +40,7 @@ public class StateAktiv extends State{
 
 
 
-    public String ausfahren() {
-        if(ticket == null){throw new TicketNichtGefundenException("Ticket nicht gefunden.");
-        }else if (ticket.getEntwertet()) {
-            LocalTime timeStamp = LocalTime.now().minusMinutes(15);
-            LocalTime uhrzeit = ticket.getUhrzeit();
-            if (uhrzeit.equals(timeStamp) || uhrzeit.isAfter(timeStamp)){
-                //Parkplatz freigeben:
-                String art = ticket.getArtDesParkplatzes();
-                ticket.getParkhaus().setAnzahlFreierParkplaetze(ticket.getParkhaus().getAnzahlFreierParkplaetze()+1);
-                //Für die speziellen Parkplätze:
-                if (art.equals("Normaler Parkplatz")) {
-                    ticket.getParkhaus().setAnzahlFreierNormalerParkplaetze((ticket.getParkhaus().getAnzahlFreierNormalerParkplaetze() + 1));
-                } else if (art.equals("E-Auto-Parkplatz")) {
-                    ticket.getParkhaus().setAnzahlFreierEAutoParkplaetze((ticket.getParkhaus().getAnzahlFreierEAutoParkplaetze() + 1));
-                } else if (art.equals("Behinderten-Parkplatz")) {
-                    ticket.getParkhaus().setAnzahlFreierBehindertenParkplaetze((ticket.getParkhaus().getAnzahlFreierBehindertenParkplaetze() + 1));
-                } else {
-                    ticket.getParkhaus().setAnzahlFreierMotorradParkplaetze((ticket.getParkhaus().getAnzahlFreierMotorradParkplaetze() + 1));
-                }
-                //ticket wird zu inaktiven tickets hinzugefügt
-                ticket.getParkhaus().getInaktiveTickets().add(ticket);
-                //ticket wird aus aktiven tickets rausgenommen
-                ticket.getParkhaus().getAktiveTickets().remove(ticket);
-
-                return"Auf Wiedersehen!";
-
-            }
-            else {
-                ticket.setEntwertet(false);
-                return"Zeit zum Ausfahren überschritten, Zeitstempel zurückgesetzt auf: " + ticket.getUhrzeit().truncatedTo(ChronoUnit.MINUTES) +". Bitte entwerten Sie das Ticket erneut am Automaten.";
-            }
-
-        }
-        else {return"Ausfahrt nur mit entwertetem Ticket möglich.";}    }
-
+    public String ausfahren() throws TicketNichtGefundenException{
+        throw new TicketNichtGefundenException("Ausfahrt nur mit entwertetem Ticket möglich.");
+    }
 }
