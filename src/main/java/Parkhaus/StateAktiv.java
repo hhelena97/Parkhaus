cb497a78a31a3518d6866aece847d6b5a408d5b0
0xKatharina <130848315+0xKatharina@users.noreply.github.com>
2023-05-25 13:17:30 +0200

    State-Gerüst ist da, Methoden müssen noch rein

--- src/main/java/Parkhaus/StateAktiv.java
+++ src/main/java/Parkhaus/StateAktiv.java
@@ -2,6 +2,23 @@ package Parkhaus;
 
 public class StateAktiv extends State{
 
+    public StateAktiv(Ticket t) {
+        super(t);
+        this.next = new StateEntwertet(t, this);
+        this.previous = null;
+    }
 
+    public double bezahlen() {
+
+
+
+        this.ticket.zustand = next;
+    }
+
+
+
+    public String ausfahren(Ticket t) {
+        return null;
+    }
 
 }
--- src/main/java/Parkhaus/StateEntwertet.java
+++ src/main/java/Parkhaus/StateEntwertet.java
@@ -1,4 +1,18 @@
 package Parkhaus;
 
 public class StateEntwertet extends State{
+    public StateEntwertet(Ticket t, State aktiv) {
+        super(t);
+        this.next = new StateInaktiv(t);
+        this.previous = aktiv;
+    }
+
+
+    public double bezahlen() {
+        return 0;
+    }
+
+    public String ausfahren(Ticket t) {
+        return null;
+    }
 }
